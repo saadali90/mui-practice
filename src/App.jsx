328ffb728b9034e14d06e975cfef8af196ab5d66
Saad Ali Khan <saad.khan@attech-ltd.com>
2024-01-22 14:57:03 +0500

    sign in template studied -- MUI started

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SignInSide from './components/Signin'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function onClickHandler() {
+    setCount((count) => count + 1);
+  }
 
   return (
     <>
@@ -18,9 +23,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> */}
+        <button onClick={onClickHandler}>Count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -28,6 +34,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <SignInSide />
     </>
   )
 }
